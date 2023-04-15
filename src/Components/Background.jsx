87e524abad4d4384943d5d66866bfad2f23d3d0c
Sandy <peacesandy04@gmail.com>
2023-04-15 14:25:35 -0700

    started styling

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Background() {
   return (
-    <div>Background</div>
+    <div
+    style={{
+        backgroundColor:'blue',
+        
+    }}
+    />
   )
 }
 
