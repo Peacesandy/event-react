fa528bee4f540477a3030e536812ad3969bd8d74
Sandy <peacesandy04@gmail.com>
2023-04-15 14:57:24 -0700

    Added background

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import Searchbar from './Components/Searchbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"
+    style={{
+      backgroundImage: `linear-gradient(to bottom, rgba(218, 112, 214), rgba(250, 128, 114), rgba(248, 222, 126))`,
+      height: '600px',
+      width: '1345px',
+      justifyContent: 'center'
+  }}        >
       <Background/>
       <Searchbar/>
     </div>
