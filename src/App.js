3c8f3d9826662874c933bce02f58b6e9f9f8f6e8
Sandy <peacesandy04@gmail.com>
2023-04-17 20:40:51 -0700

    Added functionality for profilesearch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './Components/Background'
 import './Components/Searchbar'
 import './App.css';
+import people from './Components/data'
 import Background from './Components/Background';
 import Searchbar from './Components/Searchbar';
+import React, {useState} from 'react'
+
+const ProfileSearch = () => {
+  const [profiles, setProfiles] = useState(people);
+  const {picture, firstName, lastName} = people[profiles]
+  
+}
 
 function App() {
   return (
